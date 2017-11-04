3c2e2b6e9c80c763277b310e55e79205115f0565
Raymond Chen <Sizbei@users.noreply.github.com>
2017-11-04 11:54:39 -0400

    change stream_live.jpg to photo.jpg

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ io.on('connection', function(socket) {
     if (Object.keys(sockets).length == 0) {
       app.set('watchingFile', false);
       if (proc) proc.kill();
-      fs.unwatchFile('./stream/image_stream.jpg');
+      fs.unwatchFile('./stream/photo.jpg');
     }
   });
 
@@ -47,26 +47,26 @@ function stopStreaming() {
   if (Object.keys(sockets).length == 0) {
     app.set('watchingFile', false);
     if (proc) proc.kill();
-    fs.unwatchFile('./stream/image_stream.jpg');
+    fs.unwatchFile('./stream/photo.jpg');
   }
 }
 
 function startStreaming(io) {
 
   if (app.get('watchingFile')) {
-    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+    io.sockets.emit('liveStream', 'photo.jpg?_t=' + (Math.random() * 100000));
     return;
   }
 
-  var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
+  var args = ["-w", "640", "-h", "480", "-o", "./stream/photo.jpg", "-t", "999999999", "-tl", "100"];
   proc = spawn('raspistill', args);
 
   console.log('Watching for changes...');
 
   app.set('watchingFile', true);
 
-  fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
-    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+  fs.watchFile('./stream/photo.jpg', function(current, previous) {
+    io.sockets.emit('liveStream', 'photo.jpg?_t=' + (Math.random() * 1000000));
   })
 
 }
